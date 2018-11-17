4ba1349489a00ee6a0725b19ce805fddfdaae7ab
Alfred Sporre <alfred@familjensporre.se>
2018-11-17 14:30:13 +0100

    Added hand movement

---
+++ GameJamFall2018/Assets/Player/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public PlayerInputSO PlayerInput;
+
+    public float Speed;
+
+    public GameObject LeftHand;
+    public GameObject RightHand;
+
+    public float HandRadius;
+
+    public float MoveBorder;
+
+    public GameObject GroundCheck;
+
+    public void Start() {
+
+        // Load values from player input SO
+        LeftHand.GetComponent<Hand>().InputX = PlayerInput.InputLeftX;
+        LeftHand.GetComponent<Hand>().InputY = PlayerInput.InputLeftY;
+        RightHand.GetComponent<Hand>().InputX = PlayerInput.InputRightX;
+        RightHand.GetComponent<Hand>().InputY = PlayerInput.InputRightY;
+    }
+
+    public void FixedUpdate() {
+
+        float move = 0;
+
+        move += Speed * LeftHand.GetComponent<Hand>().Move() * Time.fixedDeltaTime;
+        move += Speed * RightHand.GetComponent<Hand>().Move() * Time.fixedDeltaTime;
+
+        transform.Translate(new Vector2(Mathf.Clamp(move, - Speed * Time.fixedDeltaTime, Speed * Time.fixedDeltaTime), 0));
+    }
+
+}
---
+++ GameJamFall2018/Assets/Player/PlayerInputSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerInput1", menuName = "Player Input" )]
+public class PlayerInputSO : ScriptableObject {
+    public string InputLeftX;
+    public string InputLeftY;
+
+    public string InputRightX;
+    public string InputRightY;
+}
