5b7bfeb7efb92f638f7ddce35fb0e3e75b2c555c
ArvidSU <carlarvidsundqvist@gmail.com>
2018-11-17 20:30:44 +0100

    Added Melee

---
+++ GameJamFall2018/Assets/Weapons/Melee.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour {
+    public MeleeSO MeleeDesc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            //collision.gameObject.applyDamage(damage);
+            // Play sound.
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
