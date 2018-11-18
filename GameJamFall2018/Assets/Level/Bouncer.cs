e278bb4e6c9eff3adb838cb9934722d04fab0d97
Alfred Sporre <alfred@familjensporre.se>
2018-11-18 11:03:42 +0100

    Balanced movement and added full level

---
+++ GameJamFall2018/Assets/Level/Bouncer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour {
+
+    public float BounceForce;
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(- new Vector2(Mathf.Cos(transform.eulerAngles.z / 180 * Mathf.PI),
+                Mathf.Sin(transform.eulerAngles.z / 180 * Mathf.PI)) * BounceForce);
+        }
+    }
+
+}
---
+++ GameJamFall2018/Assets/Level/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+    public float Speed;
+
+    public GameObject Upper;
+    public GameObject Lower;
+
+    private bool up = false;
+
+    public void FixedUpdate()
+    {
+        if (up) {
+            if (transform.position.y < Upper.transform.position.y)
+            {
+                transform.Translate(0, Speed * Time.fixedDeltaTime, 0);
+
+                if (transform.position.y >= Upper.transform.position.y)
+                    up = false;
+            }
+        }
+        else
+        {
+            if (transform.position.y > Lower.transform.position.y)
+            {
+                transform.Translate(0, - Speed * Time.fixedDeltaTime, 0);
+
+                if (transform.position.y <= Lower.transform.position.y)
+                    up = true;
+            }
+        }
+    }
+
+
+}
