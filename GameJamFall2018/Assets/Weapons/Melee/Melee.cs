1530ffe251181c87e3865deec2d120364d3e850b
ArvidSU <carlarvidsundqvist@gmail.com>
2018-11-18 02:48:22 +0100

    Axe Prefab

--- GameJamFall2018/Assets/Weapons/Melee/Melee.cs
+++ GameJamFall2018/Assets/Weapons/Melee/Melee.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class Melee : MonoBehaviour {
     public MeleeSO MeleeDesc;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == "Hand")
+        {
+            transform.eulerAngles = new Vector3(0, 0, collision.gameObject.transform.eulerAngles.z);
+            transform.Rotate(new Vector3(0, 0, -90));
+
+            GetComponent<FrictionJoint2D>().enabled = true;
+            GetComponent<FrictionJoint2D>().connectedBody = collision.gameObject.GetComponent<Rigidbody2D>();
+            GetComponent<FrictionJoint2D>().anchor = MeleeDesc.GrabPosition;
+            GetComponent<FrictionJoint2D>().connectedAnchor = new Vector2(0, 0);
+
+
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+            GetComponent<Rigidbody2D>().mass = 0;
+        }
+
         if (collision.gameObject.tag == "Player")
         {
-            //collision.gameObject.ChangeHealth(damage);
-            // Play sound.
+            
+            
         }
     }
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
--- GameJamFall2018/Assets/Weapons/Melee/MeleeSO.cs
+++ GameJamFall2018/Assets/Weapons/Melee/MeleeSO.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Melee Weapon", menuName = "Weapon/Melee")]
 public class MeleeSO : ScriptableObject {
     public Sprite Image;
-    public Vector2 GripCoordinates; // Where hand grips object.
+    public Vector2 GrabPosition; // Where hand grips object.
     public int Uses;                // Amount of uses/ammo/charge.
     public int Damage;
 }
